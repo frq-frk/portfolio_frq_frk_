d676f7e6b7e4b817e71a4d622cd9c1766f461806
Farooq <mullafarooq4@gmail.com>
2024-11-29 17:32:04 +0530

    made build output dir from dist to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Change output directory from 'dist' to 'build'
+  },
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
